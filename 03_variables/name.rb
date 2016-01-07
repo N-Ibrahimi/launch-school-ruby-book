cf3329a619ea207a7eeb435aba13ffe51eb7834a
Darren Burgess <darren_b@beezwax.net>
2016-01-06 19:59:15 -0500

    add last name

--- 03_variables/name.rb
+++ 03_variables/name.rb
@@ -1,5 +1,10 @@
-puts "Type your name: "
-name = gets.chomp
+puts "Type your first name: "
+first = gets.chomp
+
+puts "Type your last name: "
+last = gets.chomp
+
+name = first + ' ' + last
 puts "Hello, #{name}"
 
 10.times do |n|
