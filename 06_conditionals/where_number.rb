7253cedd67d4cf3bf6559ecc7562065e762a7afb
Darren Burgess <darren_b@beezwax.net>
2016-01-07 21:47:44 -0500

    add case to number evaluation

--- 06_conditionals/where_number.rb
+++ 06_conditionals/where_number.rb
@@ -1,15 +1,33 @@
 puts "type a number between 0 and 100"
 num = gets.chomp.to_i
 
-if num < 0
-  message = "positive number please"
-elsif num <= 50
-  message = "between 0 and 50"
-elsif num <= 100
-  message = "between 51 and 100"
-else
-  message = "greater than 100"
+def eval_with_if(num)
+  if num < 0
+    message = "positive number please"
+  elsif num <= 50
+    message = "between 0 and 50"
+  elsif num <= 100
+    message = "between 51 and 100"
+  else
+    message = "greater than 100"
+  end
+
+  puts message
 end
 
-puts message
+def eval_with_case(num)
+  case
+    when num < 0
+      puts "positive number please"
+    when num <= 50
+      puts "between 0 and 50"
+    when num <= 100
+      puts "between 51 and 100"
+    else
+      puts "greater than 100"
+  end
+end
+
+eval_with_if(num)
+eval_with_case(num)
 
