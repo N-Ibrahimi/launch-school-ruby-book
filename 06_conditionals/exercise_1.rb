c05c19eb9af8d1c0454da2f7d3c25b8a389a7053
Darren Burgess <darren_b@beezwax.net>
2016-01-07 21:08:06 -0500

    add exercise 1

---
+++ 06_conditionals/exercise_1.rb
@@ -0,0 +1,11 @@
+(32 * 4) >= 129 # false
+
+false != !true # false read: false is not equal to not true, which is true
+
+true == 4 # false
+
+false == (847 == '874) # true read: false is equal to false
+
+(!true || (!(100/5) == 20) || ((328 / 4) == 82)) || false # true
+
+# this is true because 328/4 = 82 is true
